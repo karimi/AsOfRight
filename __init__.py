16361dfdbd9932b54306b97ed729c9afb3ddffb0
Morteza Karimi <mortza@gmail.com>
2017-08-21 22:00:32 -0400

    three.js examples

--- __init__.py
+++ __init__.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def homepage():
+    return render_template("homepage.html")
+@app.route("/other")
+def otherpage():
+    return render_template("otherpage.html")
+@app.route("/3box")
+def box():
+    return render_template("3box.html")
+@app.route("/3line")
+def line():
+    return render_template("3line.html")
+@app.route("/3loader")
+def loader():
+    return render_template("3loader.html")
 
 if __name__ == "__main__":
     app.run()
